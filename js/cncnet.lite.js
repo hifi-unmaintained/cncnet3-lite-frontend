6c6f00945f1bf836963f3138adf205e60dd5e072
Toni Spets <toni.spets@iki.fi>
2011-08-12 09:42:20 +0300

    Change API URL to the final one

--- js/cncnet.lite.js
+++ js/cncnet.lite.js
@@ -20,7 +20,7 @@
 
             var defaults = {
                 protocol: 'cnc95',
-                url: 'http://hifi.iki.fi/cncnet/lite-backend/'
+                url: 'http://cncnet.cnc-comm.com/api/'
             };
 
             var options = $.extend(defaults, options);
